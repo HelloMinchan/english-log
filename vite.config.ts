82e85a31f9b6006fdccb2fa11b7abcc2c8b075e5
HelloMinchan <wjdalscksdle@naver.com>
2025-01-28 17:34:57 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        name: "EnglishLog",
+        short_name: "EnglishLog",
         theme_color: "#ffffff",
         icons: [
           {
